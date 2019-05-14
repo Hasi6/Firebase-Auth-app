890dd612a15695764a79a303eaf8490af5f14027
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-05-14 11:53:15 +0530

    Add google popup
    Now Users can register to our system using google popups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+import { auth } from 'firebase';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'firebase-angula-auth-app';
+
+  constructor( private afAuth: AngularFireAuth){}
+
+  googleSignInViaPopup() {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider())
+    .then((userCredentials) => console.log(userCredentials));
+  }
 }
